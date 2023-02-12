4f386702aede208565926184d5e70970719b2316
Zhengxu Xia <zxxia@uchicago.edu>
2023-02-11 18:54:49 -0600

    Support MacOS in installation script.

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
 
-sudo apt update
-sudo apt install -y stow git curl gcc g++ make ripgrep
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    brew install stow git curl gcc g++ make ripgrep
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    # Assume it is Ubuntu
+    sudo apt update
+    sudo apt install -y stow git curl gcc g++ make ripgrep
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
 
 stow .
