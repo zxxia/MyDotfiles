d7b938ac65f1b4eed6f2829b63eba89e9dc85b9f
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-20 13:31:19 -0500

    Change signs in gitsigns to make it more intuitive.

--- nvim/.config/nvim/plugin/gitsigns.lua
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -1,7 +1,15 @@
 local gitsigns_status_ok, gitsigns = pcall(require, 'gitsigns')
 
 if not gitsigns_status_ok then
-    print("Warning: gitsigns not available, skipping configuration.")
+  print("Warning: gitsigns not available, skipping configuration.")
   return
 end
-gitsigns.setup()
+gitsigns.setup{
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
