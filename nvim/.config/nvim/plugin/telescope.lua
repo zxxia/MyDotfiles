56900b45bef1663839989a472625693544187cb0
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-20 17:44:36 -0500

    Create keymap for method listing in telescope.

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -25,7 +25,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fm', function()
+  builtin.lsp_document_symbols( { symbols = 'method' } )
+end, {})
 
 -- Ctrl-p = fuzzy finder
 vim.keymap.set('n', '<C-P>', function()
