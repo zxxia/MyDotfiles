f0924446c30a0d2690b1d7eda586ce5f94bdb053
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-20 22:30:51 -0500

    Fix lua indentation in null-ls setting.

--- nvim/.config/nvim/plugin/null-ls.lua
+++ nvim/.config/nvim/plugin/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    print("Warning: null-ls not available, skipping configuration.")
+  print("Warning: null-ls not available, skipping configuration.")
   return
 end
 
