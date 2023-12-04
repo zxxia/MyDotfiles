1ecb8b0910cea0ebfd9f79e7b1249edcad568a39
Zhengxu Xia <zxxia@uchicago.edu>
2023-12-03 22:04:36 -0600

    Safely select entries with <CR> in cmp completion

--- nvim/.config/nvim/plugin/cmp.lua
+++ nvim/.config/nvim/plugin/cmp.lua
@@ -55,7 +55,17 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<CR>'] = cmp.mapping({
+     i = function(fallback)
+         if cmp.visible() and cmp.get_active_entry() then
+           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+         else
+           fallback()
+         end
+       end,
+       s = cmp.mapping.confirm({ select = true }),
+       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+     }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
