1a2299bb6732303c04dea945732f341943c9f518
Zhengxu Xia <zxxia@uchicago.edu>
2024-01-03 23:48:54 -0600

    Add vimdoc in treesitter

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -8,7 +8,7 @@ require('nvim-treesitter.configs').setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {
     "bash", "c", "c_sharp", "comment", "cpp", "javascript", "latex", "lua",
-    "markdown", "python", "vim" },
+    "markdown", "python", "vim", "vimdoc"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
