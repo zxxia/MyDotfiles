e154974ce3fc9002d8b9de2295ac51ace94030cb
Zhengxu Xia <zxxia@uchicago.edu>
2024-07-16 10:22:56 -0500

    Fix tab mapping with luasnip

--- nvim/.config/nvim/plugin/cmp.lua
+++ nvim/.config/nvim/plugin/cmp.lua
@@ -70,8 +70,10 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      -- elseif luasnip.expand_or_jumpable() then
+      --   luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
